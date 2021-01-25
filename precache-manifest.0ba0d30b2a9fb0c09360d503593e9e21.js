da187a35e031074e2764b26a52965c6aebaf0177
阿栋的仓库 <2675413003@qq.com>
2021-01-25 13:47:03 +0800

    deploy

---
+++ precache-manifest.0ba0d30b2a9fb0c09360d503593e9e21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d87e4bb552c4fadd3b9",
+    "url": "/money-1-1/css/app.941d5276.css"
+  },
+  {
+    "revision": "37d0c13c5669735eb4c0abb7cf42a802",
+    "url": "/money-1-1/index.html"
+  },
+  {
+    "revision": "3d87e4bb552c4fadd3b9",
+    "url": "/money-1-1/js/app.c9f57e07.js"
+  },
+  {
+    "revision": "72bc0322c0e3cdea52d2",
+    "url": "/money-1-1/js/chunk-vendors.117c999f.js"
+  },
+  {
+    "revision": "65975ed126ff3fb8192cd1fcb55f6983",
+    "url": "/money-1-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-1/robots.txt"
+  }
+]);
